1f455779f4681db0b743f0713bf81aaff340c922
its-kumar-yash <yash-kumar.ug21@nsut.ac.in>
2023-02-12 20:12:03 +0530

    API & basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,179 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div
+        className="blur"
+        style={{
+          background: `${
+            weatherData ? BackgroundColor(weatherData) : "#a6ddf0"
+          }`,
+          top: "-10%",
+          right: "0",
+        }}
+      ></div>
+      <div
+        className="blur"
+        style={{
+          background: `${
+            weatherData ? BackgroundColor(weatherData) : "#a6ddf0"
+          }`,
+          top: "36%",
+          left: "-6rem",
+        }}
+      ></div>
+
+      <div className="content">
+        <div
+          className="form-container"
+          style={{
+            backgroundImage: `url(${
+              weatherData ? BackgroundImage(weatherData) : LakeBackground
+            })`,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="name">
+            <Animation />
+            <div className="toggle-container">
+              <input
+                type="checkbox"
+                className="checkbox"
+                id="checkbox"
+                checked={isDark}
+                onChange={toggleDark}
+              />
+              <label htmlFor="checkbox" className="label">
+                <TbMoon style={{ color: "#a6ddf0" }} />
+                <TbSun style={{ color: "#f5c32c" }} />
+                <div className="ball" />
+              </label>
+            </div>
+            <div className="city">
+              <TbMapSearch />
+              <p>{city ?? t("unknown-location")}</p>
+            </div>
+          </div>
+          <div className="search">
+            <h2
+              style={{
+                marginRight: currentLanguage === "es" || "fr" ? "10px" : "0px",
+              }}
+            >
+              {t("title")}
+            </h2>
+
+            <hr />
+
+            <form className="search-bar" noValidate onSubmit={submitHandler}>
+              <input
+                onClick={activate}
+                placeholder={active ? "" : "Explore cities weather"}
+                onChange={(e) => searchCountries(e.target.value)}
+                required
+                className="input_search"
+              />
+              <div className="list-dropdown">
+                {countryMatch &&
+                  countryMatch.map((item, index) => (
+                    <div>
+                      {/* eslint-disable-next-line no-template-curly-in-string */}
+                      <Card title={`Country: ${item}`}></Card>
+                    </div>
+                  ))}
+              </div>
+
+              <button className="s-icon">
+                <TbSearch
+                  onClick={() => {
+                    navigator.geolocation.getCurrentPositon(myIP);
+                  }}
+                />
+              </button>
+            </form>
+
+            <button
+              className="s-icon sound-toggler"
+              onClick={() => setBackgroundSoundEnabled((prev) => !prev)}
+            >
+              {backgroundSoundEnabled ? <TbVolume /> : <TbVolumeOff />}
+            </button>
+          </div>
+        </div>
+        <div className="info-container">
+          <div className="info-inner-container">
+            <div className="toggle-container">
+              <input
+                type="checkbox"
+                className="checkbox"
+                id="fahrenheit-checkbox"
+                onChange={toggleFahrenheit}
+              />
+              <label htmlFor="fahrenheit-checkbox" className="label">
+                <RiFahrenheitFill />
+                <RiCelsiusFill />
+                <div className="ball" />
+              </label>
+            </div>
+          </div>
+          {loading ? (
+            <div className="loader"></div>
+          ) : (
+            <span>
+              {weatherData.length === 0 ? (
+                <div className="nodata">
+                  <h1>{noData ?? t("no-data")}</h1>
+                  {noData === "Location Not Found" ? (
+                    <>
+                      <img
+                        src={Astronaut}
+                        alt="an astronaut lost in the space"
+                      />
+                      <p>Oh oh! We're lost in space finding that place.</p>
+                    </>
+                  ) : (
+                    <>
+                      <img
+                        src={SearchPlace}
+                        alt="a person thinking about what place to find"
+                      />
+                      <p style={{ padding: "20px" }}>
+                        Don't worry, if you don't know what to search for, try:
+                        Dhaka, Canada or maybe USA.
+                      </p>
+                    </>
+                  )}
+                </div>
+              ) : (
+                <>
+                  <h1 className="centerTextOnMobile">{t("today")}</h1>
+                  <DetailsCard
+                    weather_icon={weatherIcon}
+                    data={weatherData}
+                    soundEnabled={backgroundSoundEnabled}
+                    isFahrenheitMode={isFahrenheitMode}
+                    degreeSymbol={degreeSymbol}
+                  />
+                  <h1 className="title centerTextOnMobile">
+                    {t("more-on")} {city ?? t("unknown-location")}
+                  </h1>
+                  <ul className="summary">
+                    {weatherData.list.map((days, index) => (
+                      <SummaryCard
+                        key={index}
+                        day={days}
+                        isFahrenheitMode={isFahrenheitMode}
+                        degreeSymbol={degreeSymbol}
+                      />
+                    ))}
+                  </ul>
+                </>
+              )}
+            </span>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
