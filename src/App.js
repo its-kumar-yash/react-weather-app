286e16028990a351899578c2fd6ebc237f7943ae
its-kumar-yash <yash-kumar.ug21@nsut.ac.in>
2023-02-15 19:10:07 +0530

    Code Logic

--- src/App.js
+++ src/App.js
@@ -11,8 +11,180 @@ import {
 } from "react-icons/tb";
 import "./App.css";
 
+import DetailsCard from "./components/DetailsCard";
+import SummaryCard from "./components/SummaryCard";
+import LakeBackground from "./asset/lake-background.jpg";
+import Astronaut from "./asset/not-found.svg";
+import SearchPlace from "./asset/search.svg";
+import BackgroundColor from "./components/BackgroundColor";
+import BackgroundImage from "./components/BackgroundImage";
+import Animation from "./components/Animation";
+import "./languages/i18n";
+
+import axios from "axios";
+import { Card } from "antd";
 
 function App() {
+  //Variable declarations
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const { t, i18n } = useTranslation();
+  const [noData, setNoData] = useStete();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [weatherData, setWeatherData] = useState([]);
+  const [city, setCity] = useState();
+  const [weatherIcon, setWeatherIcon] = useState(
+    `https://openweathermap.org/img/wn/10n@2x.png`
+  );
+  const [currentLanguage, setLanguage] = useState(() => {
+    return localStorage.getItem("language") || "en";
+  });
+  const [loading, setLoading] = useState(false);
+  const [backgroundSoundEnabled, setBackgroundSoundEnabled] = useState(true);
+  const [isFahrenheitMode, setIsFahrenheitMode] = useState(false);
+  const degreeSymbol = useMemo(
+    () => (isFahrenheitMode ? "\u00b0F" : "\u00b0C"),
+    [isFahrenheitMode]
+  );
+  const [active, setActive] = useState(false);
+  const [isDark, setIsDark] = useState(false);
+
+  // code logic
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [Dark]);
+
+  //setting themee according to device
+  useEffect(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setIsDark(true);
+    }
+
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        setIsDark(event.matches);
+      });
+  }, [setIsDark]);
+
+  const toggleDark = () => {
+    setIsDark((prev) => !prev);
+  };
+
+  const activate = () => {
+    setActive(true);
+  };
+
+  useEffect(() => {
+    if (currentLanguage === "en") return;
+
+    changeLanguage(currentLanguage);
+
+    // eslint-disable-next-line
+  }, [currentLanguage]);
+
+  const toggleFahrenheit = () => {
+    setIsFahrenheitMode(!isFahrenheitMode);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    getWeather(searchTerm);
+  };
+
+  const getWeather = async (location) => {
+    setLoading(true);
+    setWeatherData([]);
+    let how_to_search =
+      typeof location === "string"
+        ? `q=${location}`
+        : `lat=${location[0]}&lon=${location[1]}`;
+
+    const url = "https://api.openweathermap.org/data/2.5/forecast?";
+    try {
+      let res = await fetch(
+        `${url}${how_to_search}&appid=${API_KEY}&units=metric&cnt=5&exclude=hourly,minutely`
+      );
+      let data = await res.json();
+      if (data.cod !== "200") {
+        setNoData("Location Not Found");
+        setCity("Unknown Location");
+        setTimeout(() => {
+          setLoading(false);
+        }, 500);
+        return;
+      }
+      setWeatherData(data);
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
+      setCity(`${data.city.name}, ${data.city.country}`);
+      setWeatherIcon(
+        `${
+          "https://openweathermap.org/img/wn/" + data.list[0].weather[0]["icon"]
+        }@4x.png`
+      );
+    } catch (error) {
+      setLoading(true);
+      console.log(error);
+    }
+  };
+
+  const myIP = (location) => {
+    const { latitude, longitude } = location.coords;
+    getWeather([latitude, longitude]);
+  };
+
+  // For the autocomplete search box- Places List
+  const [countries, setCountries] = useState([]);
+  const [countryMatch, setCountryMatch] = useState([]);
+
+  useEffect(() => {
+    const loadCountries = async () => {
+      const response = await axios.get("https://restcountries.com/v3.1/all");
+      let arr = [];
+      response.data.forEach((element) => {
+        arr.push(element.name.official);
+      });
+      setCountries(arr);
+      console.log(arr);
+    };
+
+    loadCountries();
+  }, []);
+
+  // console.log(countries);
+
+  const searchCountries = (input) => {
+    // const {value}=input.target;
+    setSearchTerm(input);
+
+    if (!input) {
+      // created if-else loop for matching countries according to the input
+      setCountryMatch([]);
+    } else {
+      let matches = countries.filter((country) => {
+        // eslint-disable-next-line no-template-curly-in-string
+        const regex = new RegExp(`${input}`, "gi");
+        // console.log(regex)
+        return country.match(regex) || country.match(regex);
+      });
+      setCountryMatch(matches);
+    }
+    // console.log(countryMatch);
+  };
+
+  // load current location weather info on load
+  window.addEventListener("load", function () {
+    navigator.geolocation.getCurrentPosition(myIP);
+  });
+
   return (
     <div className="container">
       <div
